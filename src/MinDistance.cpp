9c4d9359ff8193b65e20709bb0b53c02a9b6c655
Kilgore Trout <std.array@gmail.com>
2025-03-16 14:18:36 +0100

    Added copyrights.

--- src/MinDistance.cpp
+++ src/MinDistance.cpp
@@ -1,4 +1,9 @@
 // ----------------------------------------------------------------------------------------------------
+// Confidential and proprietary source code of BEOALEX.
+// This source code may not be used, reproduced or disclosed in any manner without permission.
+// Copyright (c) 2025, BEOALEX. All rights reserved.
+// https://github.com/beoalex
+// ----------------------------------------------------------------------------------------------------
 
 #include "Utils.hpp"
 #include "Vector3D.hpp"
